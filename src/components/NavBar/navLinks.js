05d8be7bb7803ddfcc7b07f9a70f345aa28e7569
IlsevNierop <ilsevannierop@gmail.com>
2023-06-29 10:55:40 +0200

    create navlink function for creating navbar based on authorities

---
+++ src/components/NavBar/navLinks.js
@@ -0,0 +1,92 @@
+// mogelijk in een object een dieper object wat dan een dropdown menu creeert (onder workshops - goedkeuren workshops bijvoorbeeld)
+//
+
+
+export function navLinks(authorities) {
+
+
+    if (authorities != null) {
+
+        const stringAuthorities = authorities.map((a) => {
+            return a.authority
+        });
+
+        console.log(stringAuthorities);
+
+        if (stringAuthorities.includes("ROLE_ADMIN")) {
+            return [
+                {
+                    title: "Mijn Account",
+                    link: "/test",
+                },
+                {
+                    title: "Goedkeuren Workshops",
+                    link: "/test",
+                },
+                {
+                    title: "Goedkeuren Reviews",
+                    link: "/test",
+                },
+                {
+                    title: "Workshops",
+                    link: "/test",
+                },
+                {
+                    title: "Reviews",
+                    link: "/test",
+                },
+                {
+                    title: "Boekingen",
+                    link: "/test",
+                },
+                {
+                    title: "Gebruikers",
+                    link: "/test",
+                }];
+        } else if (stringAuthorities.includes("ROLE_WORKSHOPOWNER")) {
+            return [
+                {
+                    title: "WorkshopOwner",
+                    link: "/test",
+                }, {
+                    title: "Mijn Account",
+                    link: "/test",
+                },
+                {
+                    title: "Mijn Workshops",
+                    link: "/test",
+                },
+                {
+                    title: "Reviews",
+                    link: "/test",
+                },
+                {
+                    title: "Boekingen",
+                    link: "/test",
+                },
+                {
+                    title: "Nieuwe Workshop",
+                    link: "/test",
+                }];
+        } else {
+            return [
+                {
+                    title: "Customer",
+                    link: "/test",
+                },
+                {
+                    title: "Mijn Account",
+                    link: "/test",
+                },
+                {
+                    title: "Mijn Boekingen",
+                    link: "/test",
+                },
+                {
+                    title: "Mijn Reviews",
+                    link: "/test",
+                }];
+        }
+    }
+
+}
