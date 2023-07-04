c52891950ed5479a10d41662f8388e179353c9d6
IlsevNierop <ilsevannierop@gmail.com>
2023-07-04 16:39:05 +0200

    add validation styling to input fields

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -3,23 +3,26 @@ import styles from "../InputField/InputField.module.css";
 
 function InputField({type, name, label, validation, register, errors, children, value, onChange, placeholder}) {
     return (
+            <div className={styles["input-field__column"]}>
+                <label className={styles["label__input-field"]} htmlFor={`${name}-field`}>
+                    {label}
+                    <input className={`${errors[name] ? styles["input-field__error"] : styles["input-field__none"]} ${styles["input-field"]}`}
+                           value={value}
 
-        <label className={styles["label__input-field"]} htmlFor={`${name}-field`}>
-            {label}
-            <input className={styles["input-field"]}
-                   value={value}
+                        // className={({errors[name]) => {errors[name]} ? styles['input-field__warning'] : styles['input-field']}
+                           type={type}
+                           id={`${name}-field`}
+                           {...register(name, validation)}
+                           onChange={onChange}
+                           placeholder={placeholder}
 
-                // className={({errors[name]) => {errors[name]} ? styles['input-field__warning'] : styles['input-field']}
-                   type={type}
-                   id={`${name}-field`}
-                   {...register(name, validation)}
-                   onChange={onChange}
-                   placeholder={placeholder}
-
-            />
-            {children}
-            {errors[name] && <p>{errors[name].message}</p>}
-        </label>
+                    />
+                    {children}
+                </label>
+                <div className={styles["error__row"]}>
+                {errors[name] && <p style={{ whiteSpace: 'pre-line'}} className={styles["input-field__error-message"]}>{errors[name].message}</p>}
+                </div>
+            </div>
     )
 }
 
