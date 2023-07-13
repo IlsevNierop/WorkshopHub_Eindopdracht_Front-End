b47a2eb24ef55ef440a4d15977e29cb0350a1377
IlsevNierop <ilsevannierop@gmail.com>
2023-07-13 12:25:33 +0200

    add reviews and highlighted info to workshoppage incl styling

--- src/pages/workshopPage/WorkshopPage.js
+++ src/pages/workshopPage/WorkshopPage.js
@@ -74,17 +74,21 @@ function WorkshopPage() {
             <div className={`inner-container ${styles["workshop-page__inner-container"]}`}>
                 <h1>{singleWorkshopData.title}</h1>
 
-                <article className={styles["top-part__workshop-page__overview"]}>
+                <article className={styles["top-part__workshop-page"]}>
 
                     <aside className={styles["left-side__top__workshop"]}>
 
                         <div className={styles["workshop-owner-rating"]}>
-                            <StarRating rating={singleWorkshopData.averageRatingWorkshopOwnerReviews}></StarRating>
+                            <StarRating rating={singleWorkshopData.averageRatingWorkshopOwnerReviews}
+                                        size={20}></StarRating>
                             <p>
-                                {singleWorkshopData.averageRatingWorkshopOwnerReviews} / 5 (
+                                {singleWorkshopData.averageRatingWorkshopOwnerReviews != null ? singleWorkshopData.averageRatingWorkshopOwnerReviews.toFixed(1) : 0} /
+                                5 (
                                 {singleWorkshopData.numberOfReviews === 1
                                     ? `${singleWorkshopData.numberOfReviews} review`
-                                    : `${singleWorkshopData.numberOfReviews} reviews`}
+                                    : `${singleWorkshopData.numberOfReviews != null ? singleWorkshopData.numberOfReviews : "nog geen"} reviews`
+                                }
+
                                 )
                             </p>
                         </div>
@@ -132,72 +136,79 @@ function WorkshopPage() {
                 </article>
 
                 {Object.keys(singleWorkshopData).length > 0 &&
-                    <section className={styles["bottom-part__workshop-page__overview"]}>
-                        <article className={styles["description__bottom__workshop"]}>
-                            <h4>Omschrijving workshop</h4>
+                    <>
+                        <article className={styles["description__middle-part__workshop"]}>
+                            <h4>Omschrijving van de workshop</h4>
                             <p>{singleWorkshopData.description}</p>
                         </article>
 
 
+                        <section className={styles["bottom-part__workshop"]}>
 
+                            <article className={styles["reviews__bottom__workshop"]}>
+                                <h4>Reviews</h4>
+                                {singleWorkshopData.workshopOwnerReviews.length > 0 ?
+                                    <>
+                                        <div className={styles["workshop-owner-rating"]}>
+                                            <StarRating rating={singleWorkshopData.averageRatingWorkshopOwnerReviews}
+                                                        size={26}></StarRating>
+                                            <p>
+                                                {singleWorkshopData.averageRatingWorkshopOwnerReviews.toFixed(1)} / 5 (
+                                                {singleWorkshopData.numberOfReviews === 1
+                                                    ? `${singleWorkshopData.numberOfReviews} review`
+                                                    : `${singleWorkshopData.numberOfReviews} reviews`}
+                                                )
+                                            </p>
+                                        </div>
 
-                        <article className={styles["reviews__bottom__workshop"]}>
-                            <h4>Reviews</h4>
-                            {singleWorkshopData.workshopOwnerReviews.length > 0 ?
-                                <>
-                                    <div className={styles["workshop-owner-rating"]}>
-                                        <StarRating rating={singleWorkshopData.averageRatingWorkshopOwnerReviews}></StarRating>
-                                        <p>
-                                            {singleWorkshopData.averageRatingWorkshopOwnerReviews} / 5 (
-                                            {singleWorkshopData.numberOfReviews === 1
-                                                ? `${singleWorkshopData.numberOfReviews} review`
-                                                : `${singleWorkshopData.numberOfReviews} reviews`}
-                                            )
-                                        </p>
-                                    </div>
+                                        <section className={styles["container__reviews"]}>
 
-                                    {singleWorkshopData.workshopOwnerReviews.map((review) => {
+                                            {singleWorkshopData.workshopOwnerReviews.map((review) => {
+                                                return (
+                                                    <article className={styles["container__individual-review"]}
+                                                             key={review.id}>
+                                                        <div className={styles["top-row__review"]}>
+                                                            <StarRating rating={review.rating} size={14}></StarRating>
+                                                            <p>{review.rating.toFixed(1)} / 5</p>
+                                                            <h5 className={styles["name-reviewer"]}>| {review.firstNameReviewer}</h5>
+                                                        </div>
+                                                        <p className={styles["date-review"]}>Datum
+                                                            workshop: {updateDateFormatShort(review.workshopDate)}</p>
+                                                        <h5>Workshop: {review.workshopTitle}</h5>
+                                                        <p>{review.reviewDescription}</p>
+                                                    </article>
+                                                )
+                                            })
+                                            }
+                                        </section>
+                                    </>
+                                    :
+                                    <>
+                                        <div className={styles["zero-review"]}>
+                                            <p>Er zijn nog geen reviews</p>
+                                            {/*    TODO button toevoegen om review achter te laten? */}
+                                        </div>
+                                    </>
+                                }
+                            </article>
+
+                            <div className={styles["info__bottom__workshop"]}>
+                                <h5>Belangrijk om te weten</h5>
+
+                                {singleWorkshopData.highlightedInfo &&
+                                    <ul>{(singleWorkshopData.highlightedInfo.split(".")).filter(info => info.trim() !== "").map((info) => {
                                         return (
-                                            <>
-                                            <StarRating rating={review.rating}></StarRating>
-                                                <p>{review.rating}</p>
-                                                <p>{review.firstNameReviewer}</p>
-                                                <p>Datum workshop: {updateDateFormatShort(review.workshopDate)}</p>
-                                                <h5>Workshop: {review.workshopTitle}</h5>
-                                                <p>{review.reviewDescription}</p>
-                                            </>
+                                            <li className={styles["list-item"]} key={info.slice(0, 3)}>{info}</li>
                                         )
                                     })
-                                    }
+                                    }</ul>
+                                }
+
+                            </div>
 
 
-                                </>
-                                :
-                                <>
-                                    <div className={styles["zero-review"]}>
-                                        <p>Nog geen reviews</p>
-                                        {/*    TODO button toevoegen om review achter te laten? */}
-                                    </div>
-                                </>
-                            }
-                        </article>
-
-                        <div className={styles["info__bottom__workshop"]}>
-                            <h5>Belangrijk om te weten</h5>
-
-                            {singleWorkshopData.highlightedInfo &&
-                                <ul>{(singleWorkshopData.highlightedInfo.split(".")).map((info) => {
-                                    return (
-                                        <li key={info.slice(0, 3)}>{info}</li>
-                                    )
-                                })
-                                }</ul>
-                            }
-
-                        </div>
-
-
-                    </section>
+                        </section>
+                    </>
 
                 }
 
