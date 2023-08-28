9a03d8d28987dfcaf20c988485a02093c23862ce
IlsevNierop <ilsevannierop@gmail.com>
2023-08-28 13:35:03 +0200

    remove react comments and files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from "react-router-dom";
 import AuthContextProvider from "./context/AuthContext";
 import ModalSignInProvider from "./context/ModalSigninContext";
